3e74ef86413174abbb044345f2e47d49ec9c42a9
MohanaVijayaLakshmi <mohanavijayalakshmi.v@gmail.com>
2023-10-26 12:54:04 -0400

    Final update

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -44,7 +44,7 @@ regd_users.post("/login", (req,res) => {
     req.session.authorization = {
       accessToken,username
   }
-  return res.status(200).send("User successfully logged in");
+  return res.status(200).json({message: "User successfully logged in"});
   } else {
     return res.status(208).json({message: "Invalid Login. Check username and password"});
   }
@@ -57,7 +57,10 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   const username = req.session.authorization.username;
   let book = books[isbn];
   book.reviews[username] = req.body.review;
-  res.json(book);
+  res.json({
+      message: "Book review added successfully",
+      book: book
+  });
 });
 
 regd_users.delete("/auth/review/:isbn",(req,res)=>{
@@ -65,7 +68,10 @@ regd_users.delete("/auth/review/:isbn",(req,res)=>{
     const username = req.session.authorization.username;
     delete books[isbn].reviews[username];
 
-    res.send(books[isbn]);
+    res.json({
+        message: "Book review deleted successfully",
+        book: books[isbn]
+    });
 })
 
 module.exports.authenticated = regd_users;
