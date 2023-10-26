085e4b24687238a8d2e911c9f357657bece6dd1a
MohanaVijayaLakshmi <mohanavijayalakshmi.v@gmail.com>
2023-10-26 11:25:00 -0400

    Added async await in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,20 +21,21 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books,null,4));
+  await res.promise(JSON.stringify(books,null,4));
+
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
     const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    await res.promise(books[isbn]);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author',async function (req, res) {
   //Write your code here
   const author = req.params.author;
   let book = {};
@@ -45,12 +46,12 @@ public_users.get('/author/:author',function (req, res) {
     }
         book=cur;
   })
-  res.status(200).json(book);
+  await res.promise(book);
 
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
   const title = req.params.title;
   let book = {};
@@ -61,15 +62,15 @@ public_users.get('/title/:title',function (req, res) {
     }
         book=cur;
   })
-  res.status(200).json(book);
+  await res.promise(book);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', async function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
   const book = books[isbn];
-  res.status(200).json(book.reviews);
+  await res.promise(book.reviews);
 });
 
 module.exports.general = public_users;
